81d53dfa8a48074bda82328cc61e334ec4d68705
rafin8990 <rafin8990@gmail.com>
2023-03-22 22:16:17 +0600

    id wise itemcollection dtata loaded

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nuouh7o.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -30,6 +30,13 @@ async function run (){
             const query = { categoryName: category};
             const result = await itemsCollection.find(query).toArray();
             res.send(result)
+        });
+
+        app.get('/booking/:id', async (req, res)=>{
+            const id=req.params.id;
+            const query={_id: new ObjectId(id)};
+            const result= await itemsCollection.findOne(query);
+            res.send(result);
         })
 
     }
